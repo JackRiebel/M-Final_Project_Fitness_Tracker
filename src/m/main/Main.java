2cae279146b22a138d2f3e75e53639385e837a95
Jack Riebel <71671813+JackRiebel@users.noreply.github.com>
2024-02-19 15:37:00 -0600

    Updated main

--- src/m/main/Main.java
+++ src/m/main/Main.java
@@ -3,7 +3,21 @@ package m.main;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		// Instantiate vehicleManager, perform operations based on the requirements.
+		
+		//VehicleManager vehicleManager = new VehicleManager();
+		
+		// Read vehicle data from the vehcileList.csv file and initialize objects.
+		// TODO
+		
+		// Display all vehicle information.
+		// TODO
+		
+		// Display all car information.
+		// TODO
+		
+		// Display all motorbike information.
+		// TODO
 
 	}
 }
